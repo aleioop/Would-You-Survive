347d99423cbca74f263f5ce3b7590c2cf820287f
aiywatch <aiycp35@gmail.com>
2016-07-14 22:05:29 +0000

    edit interface

--- db/migrate/20160714003142_add_comments.rb
+++ db/migrate/20160714003142_add_comments.rb
@@ -2,8 +2,7 @@ class AddComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.belongs_to :user
-      t.belongs_to :era
-      t.belongs_to :reason
+      t.belongs_to :result
       t.string :body
       t.integer :vote
     end
